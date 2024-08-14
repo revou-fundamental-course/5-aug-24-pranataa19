093fe38b84a5b116c0f86973361bbee727dc876c
pranataa19 <mhdrio19@gmail.com>
2024-08-14 16:35:39 +0700

    2.3

--- js/script.js
+++ js/script.js
@@ -1,48 +1,40 @@
 function hitungBMI() {
-    // Ambil nilai dari input
+    // get Nilai dari input
     const weight = parseFloat(document.getElementById('weight').value);
     const height = parseFloat(document.getElementById('height').value) / 100; // Konversi cm ke meter
 
-    // Hitung BMI
+    // Rumus 
     const bmi = weight / (height * height);
 
-    // Tentukan kategori BMI
+    // Kategori, Range dan Solusi
     let category = '';
     let miniDesc = '';
     let rangeBMI = '';
     let solusiBMI = '';
-    let diseases = '';
     if (bmi < 18.5) {
         category = 'Kurus';
         miniDesc = 'Anda memiliki berat badan di bawah normal.';
         rangeBMI = 'BMI Anda di bawah 18.5, ini termasuk kategori kurus.';
         solusiBMI = 'Disarankan untuk meningkatkan asupan kalori dan berkonsultasi dengan ahli gizi.';
-        diseases = '<li>Malnutrisi</li><li>Anemia</li><li>Osteoporosis</li>';
     } else if (bmi >= 18.5 && bmi < 24.9) {
         category = 'Normal';
         miniDesc = 'Anda memiliki berat badan normal.';
         rangeBMI = 'BMI Anda berada di antara 18.5 dan 24.9, ini termasuk kategori normal.';
         solusiBMI = 'Pertahankan pola makan dan gaya hidup sehat.';
-        diseases = '<li>Risiko kesehatan rendah</li>';
     } else if (bmi >= 25 && bmi < 29.9) {
         category = 'Berat badan berlebih';
         miniDesc = 'Anda memiliki berat badan berlebih.';
         rangeBMI = 'BMI Anda berada di antara 25 dan 29.9, ini termasuk kategori berat badan berlebih.';
         solusiBMI = 'Pertimbangkan untuk mengurangi asupan kalori dan meningkatkan aktivitas fisik.';
-        diseases = '<li>Diabetes tipe 2</li><li>Hipertensi</li><li>Jantung Koroner</li>';
     } else {
         category = 'Obesitas';
         miniDesc = 'Anda memiliki obesitas.';
         rangeBMI = 'BMI Anda di atas 30, ini termasuk kategori obesitas.';
         solusiBMI = 'Disarankan untuk segera berkonsultasi dengan dokter atau ahli gizi.';
-        diseases = '<li>Diabetes tipe 2</li><li>Hipertensi</li><li>Jantung Koroner</li><li>Stroke</li>';
 
     }
 
-    // Tentukan range BMI dan solusi
-
-
-    // Tampilkan hasil
+    // Meampilkan hasil
     document.getElementById('bmiValue').innerText = bmi.toFixed(2);
     document.getElementById('category').innerText = category;
     document.getElementById('miniDesc').innerText = miniDesc;
@@ -51,6 +43,6 @@ function hitungBMI() {
 }
 
 function downloadHasil() {
-    // Implementasi logika untuk mendownload hasil dalam format yang diinginkan, misalnya PDF
+    // Download
     alert('Fitur download hasil BMI belum tersedia.');
 }
